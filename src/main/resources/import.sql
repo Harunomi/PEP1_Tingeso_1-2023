fce13769a5779f3c14eadaa1f1caf901712894fc
cristhofer.parada@usach.cl <cristhofer.parada@usach.cl>
2023-05-23 02:49:28 -0400

    3 replicas

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ("01001", "Proveedor1","A","Si");
-INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ("01002", "Proveedor2","C","No");
-INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ("01003", "Proveedor3","A","No");
